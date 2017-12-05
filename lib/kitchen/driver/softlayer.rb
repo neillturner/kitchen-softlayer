d4f187b71371410ab177ab2809686a4912bba28e
Juan Carlos Alonso Holmstron <carlos.holmstron@globant.com>
2017-12-05 19:29:12 -0300

    Only ask to find server if it is not a kitchen suite naming server

--- lib/kitchen/driver/softlayer.rb
+++ lib/kitchen/driver/softlayer.rb
@@ -87,7 +87,7 @@ module Kitchen
         debug "fqdn: #{config[:fqdn]}"
         debug "server_name: #{config[:server_name]}"
         debug "server_name_prefix: #{config[:server_name_prefix]}"
-        server = create_server unless find_server(config[:fqdn])
+        server = create_server if config[:fqdn].include?(config[:default_name])  || !find_server(config[:fqdn])
         state[:server_id] = server.id
         info "Softlayer instance <#{state[:server_id]}> created."
         server.wait_for do
@@ -131,7 +131,8 @@ module Kitchen
         config[:server_name] = if config[:server_name_prefix]
                                  server_name_prefix(config[:server_name_prefix])
                                else
-                                 default_name
+                                 config[:default_name] = default_name
+                                 config[:default_name].to_s
                                end
       end
 
